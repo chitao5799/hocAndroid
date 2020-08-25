4ef80b2e9b9a4e285115ce347e3aba6cce232004
NCT99 <chitao5799@gmail.com>
2020-08-25 21:34:06 +0700

    map api

---
+++ GoogleAPI/app/src/main/java/com/example/googleapi/MapAPIActivity.java
@@ -0,0 +1,35 @@
+package com.example.googleapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapAPIActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_api);
+
+        MapFragment mapFragment= (MapFragment) getFragmentManager().findFragmentById(R.id.myMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(21.0278, 105.8342);//kinh độ, vĩ độ
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,17));//17 là chỉ số zoom của map, số càng lớn zoom càng to
+        mMap.addMarker(new MarkerOptions().position(sydney).title("hà nội, việt nam"));
+
+    }
+}
